a3527538324cc11ba0c2ba769bf8aeb94adccef6
KennyD <kenji95150@hotmail.fr>
2023-06-20 13:44:48 +0200

    Initial Commit

---
+++ CoTrip/CoTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoTripApp.swift
+//  CoTrip
+//
+//  Created by Kenny DUONG on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
